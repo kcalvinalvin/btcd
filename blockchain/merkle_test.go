c068bc37a268ebc4c8ce2d141acc746de5c165b8
Conner Fromknecht <conner@lightning.engineering>
2019-01-17 02:13:54 -0800

    blockchain: Add benchmarks

--- blockchain/merkle_test.go
+++ blockchain/merkle_test.go
@@ -5,9 +5,12 @@
 package blockchain
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
 )
 
 // TestMerkle tests the BuildMerkleTreeStore API.
@@ -19,5 +22,94 @@ func TestMerkle(t *testing.T) {
 	if !wantMerkle.IsEqual(calculatedMerkleRoot) {
 		t.Errorf("BuildMerkleTreeStore: merkle root mismatch - "+
 			"got %v, want %v", calculatedMerkleRoot, wantMerkle)
+	}
+}
+
+func makeHashes(size int) []*chainhash.Hash {
+	var hashes = make([]*chainhash.Hash, size)
+	for i := range hashes {
+		hashes[i] = new(chainhash.Hash)
+	}
+	return hashes
+}
+
+func makeTxs(size int) []*btcutil.Tx {
+	var txs = make([]*btcutil.Tx, size)
+	for i := range txs {
+		tx := btcutil.NewTx(wire.NewMsgTx(2))
+		tx.Hash()
+		txs[i] = tx
+	}
+	return txs
+}
+
+// BenchmarkRollingMerkle benches the RollingMerkleTree while varying the number
+// of leaves pushed to the tree.
+func BenchmarkRollingMerkle(b *testing.B) {
+	sizes := []int{
+		1000,
+		2000,
+		4000,
+		8000,
+		16000,
+		32000,
+	}
+
+	hashes := make([][]*chainhash.Hash, len(sizes))
+	for i, size := range sizes {
+		hashes[i] = makeHashes(size)
+	}
+
+	for i, size := range sizes {
+		name := fmt.Sprintf("%d", size)
+		b.Run(name, func(b *testing.B) {
+			benchmarkRollingMerkleSlice(b, hashes[i])
+		})
+	}
+}
+
+// BenchmarkMerkle benches the BuildMerkleTreeStore while varying the number
+// of leaves pushed to the tree.
+func BenchmarkMerkle(b *testing.B) {
+	sizes := []int{
+		1000,
+		2000,
+		4000,
+		8000,
+		16000,
+		32000,
+	}
+
+	txs := make([][]*btcutil.Tx, len(sizes))
+	for i, size := range sizes {
+		txs[i] = makeTxs(size)
+	}
+
+	for i, size := range sizes {
+		name := fmt.Sprintf("%d", size)
+		b.Run(name, func(b *testing.B) {
+			benchmarkMerkle(b, txs[i])
+		})
+	}
+}
+
+func benchmarkRollingMerkleSlice(b *testing.B, hashes []*chainhash.Hash) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		merkle := NewStump(len(hashes))
+		for _, hash := range hashes {
+			merkle.add(*hash)
+		}
+	}
+}
+
+func benchmarkMerkle(b *testing.B, txs []*btcutil.Tx) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		BuildMerkleTreeStore(txs, false)
 	}
 }
