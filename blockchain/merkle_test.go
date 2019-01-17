daf459914dbc3a9bd85745a4b812e75c94914c0d
Conner Fromknecht <conner@lightning.engineering>
2019-01-17 02:14:26 -0800

    blockchain: Add RollingMerkleTree
    blockchain: Add benchmarks
    
    blockchain: Add CalcMerkleRoot
    
    multi: replace usage of BuildMerkleTreeStore with CalcMerkleRoot

--- blockchain/merkle_test.go
+++ blockchain/merkle_test.go
@@ -5,19 +5,119 @@
 package blockchain
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
 )
 
 // TestMerkle tests the BuildMerkleTreeStore API.
 func TestMerkle(t *testing.T) {
 	block := btcutil.NewBlock(&Block100000)
-	merkles := BuildMerkleTreeStore(block.Transactions(), false)
-	calculatedMerkleRoot := merkles[len(merkles)-1]
+	calcMerkleRoot := CalcMerkleRoot(block.Transactions(), false)
+	merkleStoreTree := BuildMerkleTreeStore(block.Transactions(), false)
+	merkleStoreRoot := merkleStoreTree[len(merkleStoreTree)-1]
+
+	if calcMerkleRoot != *merkleStoreRoot {
+		t.Fatalf("BuildMerkleTreeStore root does not match "+
+			"CalcMerkleRoot, store: %v calc: %v", merkleStoreRoot,
+			calcMerkleRoot)
+	}
+
 	wantMerkle := &Block100000.Header.MerkleRoot
-	if !wantMerkle.IsEqual(calculatedMerkleRoot) {
+	if !wantMerkle.IsEqual(&calcMerkleRoot) {
 		t.Errorf("BuildMerkleTreeStore: merkle root mismatch - "+
-			"got %v, want %v", calculatedMerkleRoot, wantMerkle)
+			"got %v, want %v", calcMerkleRoot, wantMerkle)
+	}
+}
+
+func makeHashes(size int) []*chainhash.Hash {
+	var hashes = make([]*chainhash.Hash, size)
+	for i := range hashes {
+		hashes[i] = new(chainhash.Hash)
+	}
+	return hashes
+}
+
+func makeTxs(size int) []*btcutil.Tx {
+	var txs = make([]*btcutil.Tx, size)
+	for i := range txs {
+		tx := btcutil.NewTx(wire.NewMsgTx(2))
+		tx.Hash()
+		txs[i] = tx
+	}
+	return txs
+}
+
+// BenchmarkRollingMerkle benches the RollingMerkleTree while varying the number
+// of leaves pushed to the tree.
+func BenchmarkRollingMerkle(b *testing.B) {
+	sizes := []int{
+		1000,
+		2000,
+		4000,
+		8000,
+		16000,
+		32000,
+	}
+
+	hashes := make([][]*chainhash.Hash, len(sizes))
+	for i, size := range sizes {
+		hashes[i] = makeHashes(size)
+	}
+
+	for i, size := range sizes {
+		name := fmt.Sprintf("%d", size)
+		b.Run(name, func(b *testing.B) {
+			benchmarkRollingMerkleSlice(b, hashes[i])
+		})
+	}
+}
+
+// BenchmarkMerkle benches the BuildMerkleTreeStore while varying the number
+// of leaves pushed to the tree.
+func BenchmarkMerkle(b *testing.B) {
+	sizes := []int{
+		1000,
+		2000,
+		4000,
+		8000,
+		16000,
+		32000,
+	}
+
+	txs := make([][]*btcutil.Tx, len(sizes))
+	for i, size := range sizes {
+		txs[i] = makeTxs(size)
+	}
+
+	for i, size := range sizes {
+		name := fmt.Sprintf("%d", size)
+		b.Run(name, func(b *testing.B) {
+			benchmarkMerkle(b, txs[i])
+		})
+	}
+}
+
+func benchmarkRollingMerkleSlice(b *testing.B, hashes []*chainhash.Hash) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		merkle := NewStump(len(hashes))
+		for _, hash := range hashes {
+			merkle.add(*hash)
+		}
+	}
+}
+
+func benchmarkMerkle(b *testing.B, txs []*btcutil.Tx) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		BuildMerkleTreeStore(txs, false)
 	}
 }
